8930a1f21213c26da9ee59e83006686adde62a45
shawn <shawn@naturalprofessional.com>
2016-10-29 21:54:10 -0700

    Add comments components and basic bootstrap styling

--- main.js
+++ main.js
@@ -1,22 +1,76 @@
-ReactDOM.render(
-  <h1>Hello World by way of something!!</h1>,
-  document.getElementById('main-1')
-);
+// ReactDOM.render(
+//   <h1>Hello World by way of something!!</h1>,
+//   document.getElementById('main-1')
+// );
+//
+//
+// var HelloWorld = React.createClass({
+//   render: function(){
+//     return (
+//       <p>Hello Again World,
+//       <span className='label label-primary'>
+//       {this.props.name}
+//       </span>
+//       </p>
+//     );
+//   }
+// });
+//
+// ReactDOM.render(
+//   <HelloWorld name="Daniel"/>,
+//     document.getElementById('main')
+// )
 
-
-var HelloWorld = React.createClass({
-  render: function(){
+var Comment = React.createClass({
+  render: function() {
     return (
-      <p>Hello Again World,
-      <span className='label label-primary'>
-      {this.props.name}
-      </span>
-      </p>
+      <div className="panel panel-default comment">
+        <div className="panel-heading">
+          {this.props.author}
+        </div>
+        <div className="panel-body">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Ted">My first comment</Comment>
+        <Comment author="Shawn">My gazillioneth comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+      commentForm
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+      <h2>Comments</h2>
+      <CommentList />
+      <h2>Add a Comment</h2>
+      <CommentForm />
+      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld name="Daniel"/>,
-    document.getElementById('main')
-)
+  <CommentBox />,
+  document.getElementById('content')
+);
